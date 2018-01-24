29087cd9d7030264d9ad34df0ff6fa8baf9b34e0
bd-arc <bdelmaire@archriss.com>
2018-01-24 23:59:41 +0100

    feat(index): expose method `getInputRangeFromIndexes()`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Carousel from './carousel/Carousel';
 import Pagination from './pagination/Pagination';
 import ParallaxImage from './parallaximage/ParallaxImage';
+import { getInputRangeFromIndexes } from './utils/animations';
 
-export { Carousel as default, Pagination, ParallaxImage };
+export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes };
