e7f59a7fd82278a85a4bdbcf4f5ef474a2459bb9
Pavan Welihinda <pavan>
2019-02-14 11:12:13 +0530

    style issues fixed. border and height issues.

--- src/parallaximage/ParallaxImage.style.js
+++ src/parallaximage/ParallaxImage.style.js
@@ -1,20 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        overflow: 'hidden',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image: {
-        position: 'relative',
-        resizeMode: 'cover',
-        width: null,
-        height: null
-    },
-    loaderContainer: {
-        ...StyleSheet.absoluteFillObject,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    overflow: 'hidden',
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  loaderContainer: {
+      ...StyleSheet.absoluteFillObject,
+      alignItems: 'center',
+      justifyContent: 'center'
+  }
 });
