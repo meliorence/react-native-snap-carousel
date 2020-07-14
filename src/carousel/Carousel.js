235556dc4851636b268beb395d0e07b7d4bd6a3a
Gzx <574443457@qq.com>
2020-07-14 19:15:44 +0800

    Fix mi8 device cannot loop

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -818,7 +818,7 @@ export default class Carousel extends Component {
         }
 
         if (nextActiveItem === this._itemToSnapTo &&
-            scrollOffset === this._scrollOffsetRef) {
+            Math.floor(scrollOffset) === Math.floor(this._scrollOffsetRef)) {
             this._repositionScroll(nextActiveItem);
         }
 
