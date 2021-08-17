681f094cba528dc54b1e029e6d83b7e2b2d898af
itenl <nesa.wong@gmail.com>
2021-08-17 11:57:12 +0800

    fix: Remove the props allowCrossItem and use the official pagingEnabled judgment to avoid the need for developers to pass multiple props (allowCrossItem / pagingEnabled)

--- src/carousel/Carousel.tsx
+++ src/carousel/Carousel.tsx
@@ -65,7 +65,7 @@ export class Carousel<TData> extends React.Component<
       slideStyle: {},
       shouldOptimizeUpdates: true,
       useExperimentalSnap: false,
-      allowCrossItem: true,
+      pagingEnabled: false,
       useScrollView: !Animated.FlatList
   };
 
@@ -1219,6 +1219,13 @@ export class Carousel<TData> extends React.Component<
       };
   }
 
+  _isAllowCrossItem() {
+    // Note: Vertical pagination is not supported on Android by ( https://reactnative.dev/docs/scrollview#pagingenabled )
+    // if (this.props.pagingEnabled && IS_ANDROID && this.props.vertical) return false;
+    // For pagingEnabled and Vertical property works well in Android 0.62.2
+    return !this.props.pagingEnabled;
+  }
+
   _getComponentStaticProps () {
       const { hideCarousel } = this.state;
       const {
@@ -1230,8 +1237,7 @@ export class Carousel<TData> extends React.Component<
           getItemLayout,
           keyExtractor,
           style,
-          useExperimentalSnap,
-          allowCrossItem
+          useExperimentalSnap
       } = this.props;
 
       const containerStyle = [
@@ -1268,7 +1274,7 @@ export class Carousel<TData> extends React.Component<
       // - Slide animations will be off
       // - Last items won't be set as active (no `onSnapToItem` callback)
       // Recommended only with large slides and `activeSlideAlignment` set to `start` for the time being
-      const snapProps = allowCrossItem ? 
+      const snapProps = this._isAllowCrossItem() ? 
         useExperimentalSnap ?
           {
           // disableIntervalMomentum: true, // Slide ± one item at a time
