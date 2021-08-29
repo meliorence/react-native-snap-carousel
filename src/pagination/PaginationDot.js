a9ba766c7d6980c31bdddfe70c383f126eb0ee03
Umair Ashraf <umrashrf@gmail.com>
2021-08-29 16:14:46 -0400

    dropped react PropTypes

--- src/pagination/PaginationDot.js
+++ src/pagination/PaginationDot.js
@@ -12,11 +12,11 @@ export default class PaginationDot extends PureComponent {
         activeOpacity: PropTypes.number,
         carouselRef: PropTypes.object,
         color: PropTypes.string,
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        containerStyle: null,
         inactiveColor: PropTypes.string,
-        inactiveStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        inactiveStyle: null,
         index: PropTypes.number,
-        style: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        style: null,
         tappable: PropTypes.bool
     };
 
