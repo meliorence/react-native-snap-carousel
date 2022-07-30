0eb4a3cbcf277e3e6667e21429e6bb4842a5077b
Ashwinder Khurana <ashwinderk@live.co.uk>
2022-07-30 15:53:41 +0100

    Removing View.propTypes
    Signed-off-by: Ashwinder Khurana <ashwinderk@live.co.uk>

--- src/pagination/PaginationDot.js
+++ src/pagination/PaginationDot.js
@@ -12,11 +12,11 @@ export default class PaginationDot extends PureComponent {
         activeOpacity: PropTypes.number,
         carouselRef: PropTypes.object,
         color: PropTypes.string,
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        containerStyle: ViewPropTypes,
         inactiveColor: PropTypes.string,
-        inactiveStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        inactiveStyle: ViewPropTypes,
         index: PropTypes.number,
-        style: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        style: ViewPropTypes,
         tappable: PropTypes.bool
     };
 
