ed012d6551763077559b8d24dc41d534fdbdeb17
Felipe Sobral <xfelipesobral@gmail.com>
2022-08-08 15:55:46 -0300

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+import Carousel from './src/carousel/Carousel';
+import Pagination from './src/pagination/Pagination';
+import ParallaxImage from './src/parallaximage/ParallaxImage';
+import { getInputRangeFromIndexes } from './src/utils/animations';
+
+export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes };
