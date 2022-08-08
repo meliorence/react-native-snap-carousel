35274f04031a528b1822c50221451130bd37f9f5
Felipe Sobral <xfelipesobral@gmail.com>
2022-08-08 16:01:21 -0300

    index

--- example/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
