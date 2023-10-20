303a1de94a8b2d932f238b433fec6b68ba2f5e16
Stan Bondar <bondar.stan@gmail.com>
2023-10-20 14:37:33 +0300

    reverted index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import Carousel from './carousel/Carousel';
 import Pagination from './pagination/Pagination';
 import ParallaxImage from './parallaximage/ParallaxImage';
 import { getInputRangeFromIndexes } from './utils/animations';
-import * as types from './index';
 
-export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes, types };
+export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes };
