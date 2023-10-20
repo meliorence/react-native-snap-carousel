5444327c0db27f97f2ea93a7cbb36ad468d4cbfa
Stan Bondar <bondar.stan@gmail.com>
2023-10-20 14:35:38 +0300

    fixed issue with view proptypes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Carousel from './carousel/Carousel';
 import Pagination from './pagination/Pagination';
 import ParallaxImage from './parallaximage/ParallaxImage';
 import { getInputRangeFromIndexes } from './utils/animations';
+import * as types from './index';
 
-export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes };
+export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes, types };
